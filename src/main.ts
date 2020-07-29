25b2b705b4d69669e117dbbb19614fa8de4789f2
Justin Baur <136baur@gmail.com>
2020-07-29 19:56:31 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+const themeSwitcher = document.querySelector("#themeSwitcher");
+themeSwitcher.addEventListener("click", toggleTheme);
+
+startup();
+
+function startup(): void {
+    const themeKey = "theme";
+    const curTheme = localStorage.getItem(themeKey);
+
+    if (curTheme == "light") {
+        console.log("Theme is light, ew!");
+        document.body.classList.replace("theme-dark", "theme-light");
+    } else if (curTheme == "dark") {
+        console.log("Theme is dark, right on!");
+        document.body.classList.add("theme-dark", "theme-dark");
+    } else {
+        console.log("Oh, it's your first time here!");
+        localStorage.setItem(themeKey, "dark");
+    }
+}
+
+function toggleTheme() {
+    console.log("Hello from toggleTheme");
+}
